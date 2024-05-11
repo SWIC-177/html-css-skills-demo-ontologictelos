2cbf483720d281f67807eb2458e0f9312da4c30a
Mark <mark.richter93@gmail.com>
2024-05-10 20:41:20 -0500

    Add event listeners to input fields

--- main.js
+++ main.js
@@ -4,3 +4,11 @@
 // In essence what we are doing is building an array from the "inputs" of the html in my case this is the First name, last name, email, and phone number forms
 const inputs = [...Array.from(document.querySelectorAll("input"))];
 console.log(inputs);
+
+//event listener
+// this listens for the event of essentially moving focus
+inputs.forEach((el) => {
+  el.addEventListener("blur", (e) => {
+    console.log("blur event", e.target.id);
+  });
+});
