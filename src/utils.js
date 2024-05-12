1eac45096a4ce17d9a9100ef673b10da80d80883
Mark <mark.richter93@gmail.com>
2024-05-11 19:43:56 -0500

    Add function to render error

--- src/utils.js
+++ src/utils.js
@@ -34,3 +34,19 @@ export const hideError = (el) => {
   el.parentNode.querySelector(".error")?.classList.remove("is-error");
 };
 
+export const renderError = (el, msg) => {
+  const elParent = el.parentNode;
+
+  // Did we already render an error?
+  const errorEl = elParent.querySelector(".error");
+
+  // If so, add the class to show the error message
+  if (errorEl) errorEl.classList.add("is-error");
+  // Otherwise, add the error message from scratch
+  else {
+    const errorElement = document.createElement("p");
+    errorElement.className = "error is-error";
+    errorElement.textContent = msg;
+    elParent.appendChild(errorElement);
+  }
+};
