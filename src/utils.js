26127e3be2e6cc8ea8a1b8b78f7d9b9eb623aa18
Mark <mark.richter93@gmail.com>
2024-05-11 19:44:18 -0500

    Import validator from validator

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,25 @@
+import validator from "validator";
+
 export const ERRORS = [
-    {
+  {
     id: "first name",
-        msg: "Please enter first name.",
-        validate(val) {
+    msg: "Please enter first name.",
+    validate(val) {
       return val.length > 2;
     },
+  },
+  {
+    id: "last name",
+    msg: "Please enter last name.",
+    validate(val) {
+      return val.length > 3;
     },
-    {
-        id: "last name",
-        msg: "Please enter last name.",
-        validate(val){
-            return val.length > 3;
-        }
-
-    },
-    {
-        id: "email",
-        msg: "Please enter your email.",
-        validate(val){
-            return val.includes("@");
+  },
+  {
+    id: "email",
+    msg: "Please enter your email.",
+    validate(val) {
+      return val.includes("@");
     },
   },
   {
